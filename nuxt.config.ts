c0d65cb71d02b67f352aeef2c13c120a8e653f85
Tsogtbayar <tsogtbayar@chimege.com>
2026-06-17 10:44:31 +0800

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/content', '@nuxt/fonts', '@nuxt/image'],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
+})
