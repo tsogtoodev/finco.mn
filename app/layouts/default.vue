76c27ac387d6478a159299b003708a5f8791a6a2
Tsogtbayar <tsogtbayar@chimege.com>
2026-08-13 15:33:59 +0800

    chore: added fps meter to dev

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -19,6 +19,8 @@ const showFloatingActions = computed(() => route.meta.floatingActions !== false)
     <SiteFooter />
     <FloatingActions v-if="showFloatingActions" />
     <AutoNextNews />
-    <FpsMeter />
+    <DevOnly>
+      <FpsMeter />
+    </DevOnly>
   </div>
 </template>
